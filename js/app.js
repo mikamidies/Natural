87da78a69b455c53d152a13e3ee4ea0b6339959e
mikamidies <mikamidies04@gmail.com>
2022-04-21 14:44:11 +0500

    Cursor effect added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+const cursor = document.getElementById('cursor')
+
+document.addEventListener('mousemove', function(e){
+    cursor.style.cssText = 'left: ' + e.clientX + 'px; top: ' + e.clientY + 'px;';
+});
+
 var swiper = new Swiper(".homeSwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
