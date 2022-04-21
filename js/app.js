12197bf3d2a5333a86d2938e1094d5204d039d3b
mikamidies <mikamidies04@gmail.com>
2022-04-21 14:26:32 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".homeSwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
